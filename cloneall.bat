54d42c747b135c5a47085b76b3f070dda6d2c5d5
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-05-30 17:07:43 +0530

    Create cloneall.bat

---
+++ cloneall.bat
@@ -0,0 +1,9 @@
+gh repo clone keshavsoft/JSONDataSupply
+
+gh repo clone keshavsoft/AdminApi
+gh repo clone keshavsoft/KStaticFiles
+gh repo clone keshavsoft/JsonApi
+gh repo clone keshavsoft/JSONUser
+gh repo clone keshavsoft/JSONUtility
+gh repo clone keshavsoft/JSONReports
+gh repo clone keshavsoft/GarmentsCloud
